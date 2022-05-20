d9f3825c30137c6fefce6fd1a9479300d6acd95d
Sravan13 <chithari.sravan@gmail.com>
2022-05-20 09:13:23 +0530

    added white listed pattern and changed email and phone number to uniqueness

--- vire-controller/src/main/resources/db/migration/V1_2__init.sql
+++ vire-controller/src/main/resources/db/migration/V1_2__init.sql
@@ -1,2 +1,3 @@
 ALTER TABLE profile ADD COLUMN first_login VARCHAR(10) NOT NULL AFTER gender;
 ALTER TABLE profile ADD COLUMN file_id varchar(191) NULL AFTER date_of_birth;
+AlTER TABLE profile ADD UNIQUE (email_id, mobile_number);
