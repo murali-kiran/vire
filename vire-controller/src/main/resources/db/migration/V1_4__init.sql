db348cc6098c6813fab0b6f13ee3ffd82dea5ee5
gmuralidharraju <gmuralidharraju@gmail.com>
2022-06-11 11:27:04 +0530

    Added SQL script

--- vire-controller/src/main/resources/db/migration/V1_4__init.sql
+++ vire-controller/src/main/resources/db/migration/V1_4__init.sql
@@ -1,13 +1,11 @@
---ALTER TABLE profile ALTER COLUMN date_of_birth SET null;
-ALTER TABLE profile ALTER COLUMN aadhar SET DEFAULT null;
---ALTER TABLE profile ALTER COLUMN is_aadhar_verified SET DATA TYPE TINYINT(1);
-ALTER TABLE profile MODIFY COLUMN is_aadhar_verified TINYINT(1) null;
---ALTER TABLE profile ALTER COLUMN is_aadhar_verified SET DEFAULT null;
-ALTER TABLE personal_profile ALTER COLUMN blood_donate_willingness SET DEFAULT null;
+ALTER TABLE profile ADD COLUMN profile_type VARCHAR(45) NULL AFTER weightage;
+ALTER TABLE profile CHANGE COLUMN aadhar aadhar VARCHAR(255) NULL;
+ALTER TABLE profile CHANGE COLUMN is_aadhar_verified is_aadhar_verified TINYINT(1) NULL DEFAULT 0 ;
+ALTER TABLE personal_profile CHANGE COLUMN blood_donate_willingness blood_donate_willingness VARCHAR(255) NULL;
 
-ALTER TABLE firm_profile ALTER COLUMN field_of_business SET DEFAULT null;
-ALTER TABLE firm_profile ALTER COLUMN product_or_service SET DEFAULT null;
-ALTER TABLE firm_profile ALTER COLUMN firm_address_id  SET DEFAULT null;
+ALTER TABLE firm_profile CHANGE COLUMN field_of_business field_of_business VARCHAR(255) NULL;
+ALTER TABLE firm_profile CHANGE COLUMN product_or_service product_or_service VARCHAR(255) NULL;
+ALTER TABLE firm_profile CHANGE COLUMN firm_address_id firm_address_id BIGINT NULL;
 
-ALTER TABLE profile ADD COLUMN profile_type varchar(50) null AFTER weightage;
+
 
