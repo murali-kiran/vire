69dcc71cab10374bcc6ea0a3a5ff51eaba041a31
Sravan13 <chithari.sravan@gmail.com>
2022-08-09 22:12:35 +0530

    Update AdminPortalController.java

--- vire-controller/src/main/java/com/vire/controller/AdminPortalController.java
+++ vire-controller/src/main/java/com/vire/controller/AdminPortalController.java
@@ -2,21 +2,32 @@ package com.vire.controller;
 
 import com.vire.constant.VireConstants;
 import com.vire.model.response.AdminHomeResponse;
+import com.vire.model.response.MinimalProfileResponse;
 import com.vire.service.AdminPortalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = VireConstants.ADMIN_PORTAL_API)
+@CrossOrigin
 public class AdminPortalController {
 
-    @Autowired
-    AdminPortalService adminPortalService;
+  @Autowired AdminPortalService adminPortalService;
 
-    @GetMapping(value = "/homepage")
-    public AdminHomeResponse getHomePage(){
-        return adminPortalService.getHomePageDetails();
-    }
+  @GetMapping(value = "/homepage")
+  public AdminHomeResponse getHomePage() {
+    return adminPortalService.getHomePageDetails();
+  }
+
+  @GetMapping(value = "/users")
+  public List<MinimalProfileResponse> getUsers() {
+    return adminPortalService.getAllUsers();
+  }
+
+
 }
