b8f5c706e366614907591126b1764e951403b959
Sravan13 <chithari.sravan@gmail.com>
2022-08-12 22:14:01 +0530

    Update AdminPortalController.java

--- vire-controller/src/main/java/com/vire/controller/AdminPortalController.java
+++ vire-controller/src/main/java/com/vire/controller/AdminPortalController.java
@@ -2,13 +2,12 @@ package com.vire.controller;
 
 import com.vire.constant.VireConstants;
 import com.vire.model.response.AdminHomeResponse;
-import com.vire.model.response.MinimalProfileResponse;
+import com.vire.model.response.ProfileResponse;
 import com.vire.service.AdminPortalService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,9 +24,20 @@ public class AdminPortalController {
   }
 
   @GetMapping(value = "/users")
-  public List<MinimalProfileResponse> getUsers() {
+  public List<ProfileResponse> getUsers() {
     return adminPortalService.getAllUsers();
   }
 
+  @GetMapping(value = "/deleteProfile/{profileId}")
+  public ResponseEntity<ProfileResponse> deleteProfile(@PathVariable("profileId") String profileId) {
+    var minimalProfile = adminPortalService.deleteProfile(Long.valueOf(profileId));
+    return minimalProfile.isPresent() ? new ResponseEntity<>(minimalProfile.get(), HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+  }
+
+  @GetMapping(value = "/blockProfile/{profileId}")
+  public ResponseEntity<Boolean>  blockProfile(@PathVariable("profileId") String profileId,@RequestParam("isBlocked") Boolean isBlocked) {
+     var blockedStatus = adminPortalService.blockProfile(Long.valueOf(profileId),isBlocked);
+     return new ResponseEntity<>(blockedStatus, HttpStatus.OK);
+  }
 
 }
