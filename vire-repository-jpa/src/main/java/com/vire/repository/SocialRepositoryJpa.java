f52af57e74d38d1e297deb9a372e9e6ebab8f729
Sravan13 <chithari.sravan@gmail.com>
2022-10-15 17:41:15 +0530

    Changed the todays count query

--- vire-repository-jpa/src/main/java/com/vire/repository/SocialRepositoryJpa.java
+++ vire-repository-jpa/src/main/java/com/vire/repository/SocialRepositoryJpa.java
@@ -19,6 +19,6 @@ public interface SocialRepositoryJpa extends JpaRepository<SocialDao, Long> , Jp
     @Query(value="SELECT s.* FROM t_social s JOIN t_social_post_send_to st ON st.social_id = s.social_id WHERE st.type='Community' and st.value = :communityId ORDER BY s.updated_time DESC", nativeQuery = true)
     List<SocialDao> findByCommunity(Long communityId);
 
-    @Query(value = "select count(*) from profile where DATE(FROM_UNIXTIME(created_time/1000)) = CURDATE()",nativeQuery = true)
+    @Query(value = "select count(*) from t_social where DATE(FROM_UNIXTIME(created_time/1000)) = CURDATE()",nativeQuery = true)
     int getTodayCreatedSocialPosts();
 }
